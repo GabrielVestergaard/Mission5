2b8f884a3b9cf847f6fb1ecdc9e295050892b101
Gabriel <vest27@byu.edu>
2026-02-13 15:39:37 -0700

    new app

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ManchesterUnitedApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
